e6a7dcf6c5076f2416cbe682cb3492e185b37abc
imjangkar <imjangkar@gmail.com>
2024-08-25 22:45:59 +0700

    add debug mode

--- src/albibong/__main__.py
+++ src/albibong/__main__.py
@@ -1,6 +1,12 @@
+import sys
+
 from albibong import main
 from albibong.classes.logger import use_logger
+from albibong.classes.utils import Utils
 
 if __name__ == "__main__":
     use_logger(True)
+
+    if sys.argv[-1] == "--debug":
+        Utils.get_user_specifications("source code")
     main(useWebview=False)
